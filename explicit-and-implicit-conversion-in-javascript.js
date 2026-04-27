f2231222b089744712b1e731f44e61ad297a0a01
Rekha <rekhavs003@gmail.com>
2026-04-26 22:27:48 -0500

    completed debugging and added type conversion examples

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -14,5 +14,30 @@ let value = "123";
 let converted = Number(value);
 console.log("Before:", typeof value, "After:", typeof converted); 
 
-let test = Number("hello");
-console.log("Edge Case (NaN):", test);
+
+let result = Number("5") - 2;
+console.log("The result is: " + result);
+
+let isValid = "false" === "true"; 
+if (isValid) {
+    console.log("This is valid!");
+} else {
+    console.log("This is not valid!"); 
+  }
+
+let age = "25";
+let totalAge = Number(age) + 5;
+console.log("Total Age: " + totalAge);
+
+let score = 10;
+let bonus = null; 
+let finalScore = score + bonus;
+console.log("--- Implicit Example ---");
+console.log("Before: bonus is", typeof bonus); 
+console.log("After calculation (10 + null):", finalScore, "Type is", typeof finalScore);
+
+let input = "not-a-number";
+let convertedInput = Number(input); 
+console.log("--- Explicit Example ---");
+console.log("Before: input is", typeof input);
+console.log("After Number() conversion:", convertedInput, "Type is", typeof convertedInput); 
